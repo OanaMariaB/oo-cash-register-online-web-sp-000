bf210edf5269feeaee44ba65e9247a04d899743d
Oana Berentan <oana.berentan@yahoo.com>
2019-06-10 21:09:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,14 +4,20 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-
+    @items = []
   end
 
-  def add_item(title, price, quantity = 1)
-      @total = title * price
-        if quantity > 1
-          @total = title * price * quantity
-        end
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+     if quantity > 1
+      counter = 0
+     while counter < quantity
+       @items << item
+       counter += 1
+     end
+   else
+     @items << item
+   end
   end
 
 end
